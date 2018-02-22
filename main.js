fa17e6e2cccad0a84d8f8e713cafa7fb465ac1eb
Thram Cortapapel <thram@Thrams-MacBook-Pro.local>
2018-02-23 01:17:23 +1300

    feat($setup): Setup Parcel + React + Babel + Electron

---
+++ main.js
@@ -0,0 +1,52 @@
+// Basic init
+const { app, BrowserWindow } = require('electron'); //eslint-disable-line
+const path = require('path');
+const url = require('url');
+
+// Let electron reloads by itself when webpack watches changes in ./app/
+if (process.env.ELECTRON_START_URL) {
+  require('electron-reload')(__dirname); //eslint-disable-line
+}
+
+// To avoid being garbage collected
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, './build/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    });
+
+  mainWindow.loadURL(startUrl);
+
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ src/containers/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <h1>Y ahora le damos rosca</h1>;
+
+export default App;
